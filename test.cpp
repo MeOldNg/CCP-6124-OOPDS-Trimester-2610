572c640cc9676376f2c1bdcd040c65b61de364a5
SEEyou06 <vivoy81.wyaness@gmai.com>
2026-06-02 10:44:54 +0800

    Build the outline

---
+++ test.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Register {
+    private:
+    
+    public:
+
+};
+
+class GeneralRegister : public Register {
+    private:
+
+    public:
+
+};
+
+class ProgramCounter {
+    private:
+
+    public:
+
+};
+
+class CPU {
+    private:
+
+    public:
+
+};
+
+// Test
+int main() {
+    CPU cpu;
+
+
+    return 0;
+}
