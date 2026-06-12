834fbbad3afb9433981bee86eff9e20ad206c824
SEEyou06 <vivoy81.wyaness@gmai.com>
2026-06-12 17:10:01 +0800

    Build CPU

--- test.cpp
+++ test.cpp
@@ -26,7 +26,7 @@ class Register {
 class GeneralRegister : public Register {
     private:
         string name; // R0, R1, ...
-        
+
     public:
         GeneralRegister(string n = "") {
             name = n;
@@ -47,14 +47,42 @@ class ProgramCounter {
         unsigned char pc;  // Range 0-255
 
     public:
-
+        ProgramCounter() {
+            pc = 0;
+        }
+        
+        unsigned char getPC() const {
+            return pc;
+        }
+        
+        void increment() {
+            pc++;
+        }
+        
+        void reset() {
+            pc = 0;
+        }
 };
 
+// CPU
 class CPU {
     private:
+        GeneralRegister registers[8]; // R0-R7
+        ProgramCounter pc;
 
     public:
-
+        CPU() {
+            // Initialize 8 register
+            registers[0].setName("R0");
+            registers[1].setName("R1");
+            registers[2].setName("R2");
+            registers[3].setName("R3");
+            registers[4].setName("R4");
+            registers[5].setName("R5");
+            registers[6].setName("R6");
+            registers[7].setName("R7");
+            
+        }
 };
 
 // Test
