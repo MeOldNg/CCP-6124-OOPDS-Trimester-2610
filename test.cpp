02c7ab4e87bdef75d235815537411425e3920552
SEEyou06 <vivoy81.wyaness@gmai.com>
2026-06-12 17:06:14 +0800

    Add program counter

--- test.cpp
+++ test.cpp
@@ -2,22 +2,49 @@
 #include <string>
 using namespace std;
 
+// Register
 class Register {
     private:
-    
-    public:
+        signed char value;  // -128 to 127
 
+    public:
+        Register() {
+            value = 0;
+        }
+        
+        signed char getValue() const {
+            return value;
+        }
+        
+        void setValue(int v) {
+            value = static_cast<signed char>(v);
+            // Flag update logic add here later
+        }
 };
 
+// General Register
 class GeneralRegister : public Register {
     private:
-
+        string name; // R0, R1, ...
+        
     public:
-
+        GeneralRegister(string n = "") {
+            name = n;
+        }
+        
+        string getName() const {
+            return name;
+        }
+        
+        void setName(string n) {
+            name = n;
+        }
 };
 
+// Program Counter
 class ProgramCounter {
     private:
+        unsigned char pc;  // Range 0-255
 
     public:
 
